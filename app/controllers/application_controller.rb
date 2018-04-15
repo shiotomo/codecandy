723db13cc200d0fcf2f0a7dfe7939855ca1ba546
shiotomo <t.shiota16@gmail.com>
2018-04-16 01:22:47 +0900

    user_signed_inヘルパーを作成した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :sign_in?
 
   private
   # ログインしているユーザの情報を提示するメソッド
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # サインインしているかどうかを確認するメソッド
+  def user_signed_in?
+    if session[:user_id]
+      return true
+    else
+      return false
+    end
   end
 
   # アクセス制限に利用するメソッド
--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,5 @@
 class WelcomesController < ApplicationController
   def index
+    redirect_to lessons_path if user_signed_in?
   end
 end
