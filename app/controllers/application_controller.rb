b96e26889501b242de3e43f4bfeb1d5ea6f089a1
shiotomo <t.shiota16@gmail.com>
2018-04-16 01:23:55 +0900

    user_signed_inヘルパーの設定を一部修正した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
-  helper_method :sign_in?
+  helper_method :user_signed_in?
 
   private
   # ログインしているユーザの情報を提示するメソッド
