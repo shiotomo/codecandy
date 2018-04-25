b16358aac6b1575e1eca2ac138c1a1f7827814e5
shiotomo <t.shiota16@gmail.com>
2018-04-25 16:05:14 +0900

    Lessonモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415131307) do
+ActiveRecord::Schema.define(version: 20180425070347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
