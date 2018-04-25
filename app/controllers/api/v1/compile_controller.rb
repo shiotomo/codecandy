033577ba1e3dfa579e7d92462b4b45d23f4e3c8d
shiotomo <t.shiota16@gmail.com>
2018-04-25 15:51:45 +0900

    UIの変更を行った

--- app/controllers/api/v1/compile_controller.rb
+++ app/controllers/api/v1/compile_controller.rb
@@ -8,17 +8,10 @@ class Api::V1::CompileController < ApplicationController
     language = params[:language]
     source_code = params[:source_code]
     input = params[:input]
-
-    puts "============>"
-    p source_code.encoding
-    # source_code.force_encoding("ASCII-8BIT")
-    p source_code.encoding
-
     container = CodeCandy::Container.new
-    puts 1
+
     result = container.init(language, source_code, input)
 
-    p result
     result[:stdout] = result[:stdout].force_encoding("UTF-8")
 
     render json: result
