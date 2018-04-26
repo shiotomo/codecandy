b4651b4821be96121c1f9776bfd098258f83d742
shiotomo <t.shiota16@gmail.com>
2018-04-26 13:33:47 +0900

    admin_usersコントローラを作成した

---
+++ app/controllers/admin_users_controller.rb
@@ -0,0 +1,2 @@
+class AdminUsersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :codes, only: :index
   resources :lessons, only: [:index, :show]
   resources :questions
+  resources :admin_users
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signout' => "sessions#destroy", as: :signout
