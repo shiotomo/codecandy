a9e5b0243df1c727c50593159d05a3da6c5a872a
shiotomo <t.shiota16@gmail.com>
2018-04-27 13:35:39 +0900

    isuue #2 timeoutライブラリを用いて解決した

--- lib/code_candy/container.rb
+++ lib/code_candy/container.rb
@@ -3,16 +3,26 @@ require 'fileutils'
 require 'open3'
 require 'docker'
 require 'docker-api'
+require 'timeout'
 
 module CodeCandy
   class Container
+    # 引数
+    # language: どの言語で動かすプログラムなのかを格納してある
+    # source_code: 実行するプログラムが格納されている
+    # input: 標準入力に使う値が格納されている
+    # 返り値
+    # return_params
     def init(language, source_code, input)
+      # 現在の時間を格納
       exec_time = Time.now.to_f
 
+      # ファイルを作成する準備
       source_file = 'main'
       input_file  = 'input'
       work_dir     = "workspace_#{exec_time}"
 
+      # 実行する言語ごとにファイルの名前を確定させ、実行コマンドを設定する
       case language
       when 'Ruby'
         source_file += '.rb'
@@ -26,6 +36,7 @@ module CodeCandy
         exec_cmd = "cc -Wall -o #{filename_id} #{source_file} && ./#{filename_id}"
       end
 
+      # コンテナを作成
       container = Docker::Container.create(
         name: "test_#{exec_time}",
         Image: 'codecandy/compile',
@@ -40,41 +51,58 @@ module CodeCandy
       )
 
       # debug
-      p work_dir
+      # p work_dir
 
+      # Open3を利用してディレクトリを作成＆権限の変更
       Open3.popen3("mkdir /tmp/#{work_dir} && chmod 777 /tmp/#{work_dir}") do |i, o, e, w| 
         i.close
         o.each do |line| p line end
         e.each do |line| p line end
       end
 
+      # プログラムのファイルの作成
       File.open("/tmp/#{work_dir}/#{source_file}", "w") do |f|
         source_code.split('\n').each do |line|
           f.puts(line)
         end
       end
 
+      # 標準入力用のファイルの作成
       File.open("/tmp/#{work_dir}/#{input_file}", "w") do |f|
         input.split('\n').each do |line|
           f.puts(line)
         end
       end
 
-      container.start
-      container_cmd = "cd /workspace && /usr/bin/time -q -f \"%e\" -o /workspace/time.txt timeout 3 #{exec_cmd} < #{input_file}" 
-      res = container.exec(['bash', '-c', container_cmd])
-      container.stop
-      container.delete(force: true)
+      # コンテナを利用してプログラムを実行
+      return_params = {}
+      begin
+        Timeout.timeout(3) do
+          # === 実行ここから ===
+          container.start
+          container_cmd = "cd /workspace && /usr/bin/time -q -f \"%e\" -o /workspace/time.txt timeout 3 #{exec_cmd} < #{input_file}" 
+          res = container.exec(['bash', '-c', container_cmd])
+          container.stop
+          container.delete(force: true)
+          # === 実行ここまで ===
+          # 実行にかかった時間をproc_timeに格納
+          proc_time = ""
+          File.open("/tmp/#{work_dir}/time.txt", "r") do |f|
+            proc_time = f.gets
+          end
+          proc_time += "[s]"
 
-      proc_time = ""
-      File.open("/tmp/#{work_dir}/time.txt", "r") do |f|
-        proc_time = f.gets
+          FileUtils.rm_r("/tmp/#{work_dir}")
+
+          return_params = {stdout: res[0].join(''), stderr: res[1].join(''), time: proc_time, exit_code: res[2]}
+
+        end
+      rescue Timeout::Error
+        container.stop
+        container.delete(force: true)
+        puts "hogehoge"
+        return_params = {stdout: "Time out!",stderr: "Time out!", time: "", exit_code: 128}
       end
-      proc_time += "[s]"
-
-      FileUtils.rm_r("/tmp/#{work_dir}")
-
-      return_params = {stdout: res[0].join(''), stderr: res[1].join(''), time: proc_time, exit_code: res[2]}
 
       return return_params
     end
