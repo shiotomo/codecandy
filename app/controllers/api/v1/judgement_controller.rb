c17ba5479102cc89de77d2d32495c0bef6e98faf
shiotomo <t.shiota16@gmail.com>
2018-05-03 20:51:23 +0900

    rubocopを追加した

--- app/controllers/api/v1/judgement_controller.rb
+++ app/controllers/api/v1/judgement_controller.rb
@@ -1,3 +1,4 @@
+# 提出されたプログラムを実行し、問題との正誤を判断するAPI
 require './lib/code_candy/container'
 
 class Api::V1::JudgementController < ApplicationController
@@ -22,11 +23,14 @@ class Api::V1::JudgementController < ApplicationController
     answer_flag = true
 
     question.answers.each do |answer|
+      # コンテナに言語、ソースコード、標準入力を与えて提出されたプログラムを実行する
       result = container.init(language, source_code, answer.input)
       result[:stdout] = result[:stdout].force_encoding("UTF-8")
 
+      # 標準出力改行コードを挿入する
       output = answer.output + "\n"
 
+      # 正解だったらanswer_flagをtrueに、違う場合はfalseにしてループを抜ける
       if result[:stdout] == output || result[:stdout] == answer.output
         answer_flag = true
       else
@@ -35,12 +39,14 @@ class Api::V1::JudgementController < ApplicationController
       end
     end
 
+    # result[:answer]に結果を格納する
     if answer_flag
       result[:answer] = "正解"
     else
       result[:answer] = "不正解"
     end
 
+    # UTF-8にencodeする
     result[:stdout] = result[:stdout].force_encoding("UTF-8")
     result[:stderr] = result[:stderr].force_encoding("UTF-8")
 
