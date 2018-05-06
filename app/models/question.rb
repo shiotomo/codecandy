a40035dffe3ba488ce582b2fda6d4e27a472948e
shiotomo <t.shiota16@gmail.com>
2018-05-07 04:57:32 +0900

    不要なvalidateを削除した

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ApplicationRecord
   validates :title, presence: true
 
   has_many :results, dependent: :destroy
-  has_many :question_programs, dependent: :destroy
   has_many :answers, dependent: :destroy, inverse_of: :question
   accepts_nested_attributes_for :answers, allow_destroy: true
 end
