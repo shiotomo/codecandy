11ec0b21e0b352de60e93b8338c607e0b7c012fd
shiotomo <t.shiota16@gmail.com>
2018-05-09 04:43:21 +0000

    #10 ファイルをa.txtで保存できるようにした

--- vendor/assets/javascripts/ui.js
+++ vendor/assets/javascripts/ui.js
@@ -35,12 +35,13 @@ function setEditorLanguage(language) {
   aceEditor.getSession().setMode("ace/mode/" + mode);
 }
 
-$('#language').val('ruby');
+// $('#language').val('ruby');
 setEditorLanguage('ruby');
 $('#language').on("change", (e) => {
   setEditorLanguage(this.value);
 });
 
+// ファイルから呼び出す
 $('#load_file').on('change', (e) => {
   const { target } = e;
   const { files } = target;
@@ -54,4 +55,17 @@ $('#load_file').on('change', (e) => {
 
 $('#load_button').on('click', (e) => {
   $('#load_file').click();
+});
+
+// ファイルに保存する
+$('#save_button').on('click', (e) => {
+  const blob = new Blob([aceEditor.getValue()], { type:'text/plain' });
+  const a = Object.assign(document.createElement('a'), {
+    href:URL.createObjectURL(blob),
+    target:'_blank',
+    download:'a'
+  })
+  document.body.appendChild(a);
+  a.click();
+  a.parentNode.removeChild(a);
 });
