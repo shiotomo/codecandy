78e39e43c0ef632d7e792b50933dad3f9fae6ea8
shiotomo <t.shiota16@gmail.com>
2018-05-17 20:27:59 +0000

    lessonsとsectionsにindexページを追加した

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,6 +1,10 @@
 class SectionsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    categories = Category.all.order(created_at: 'asc')
+  end
+
   def show
     @category = Category.friendly.find(params[:id])
   end
