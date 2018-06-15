b3bfa5a220f0804c1a9b39fbecaa0d7f4f119b53
shiotomo <t.shiota16@gmail.com>
2018-06-15 13:32:13 +0900

    sections/indexにページネーション機能を追加した #27

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,14 +8,12 @@ class LessonsController < ApplicationController
   def show
     @question = Question.friendly.find(params[:id])
     answer = @question.answers.order(created_at: 'asc').first
+
     if answer.nil?
       @input = ""
-    else
-      @input = answer.input
-    end
-    if answer.nil?
       @output = ""
     else
+      @input = answer.input
       @output = answer.output
     end
   end
