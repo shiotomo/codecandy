34169841c48639b143cefdc9194e6454ca3b5c15
shiotomo <t.shiota16@gmail.com>
2018-07-27 02:44:08 +0900

    tops/indexにステータスを表示するようにした

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,11 @@ class TopsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @results = Result.where(user_id: current_user.id)
+
+    @analysis = Hash.new(0)
+    @results.each do |result|
+      @analysis[result.language] += 1
+    end
   end
 end
