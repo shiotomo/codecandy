98b0ec64e034f23ee06b729ee77ec30137a19f54
shiotomo <t.shiota16@gmail.com>
2018-07-27 03:18:10 +0900

    tops/indexを更新した

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,7 @@ class TopsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @questions = Question.all
     @results = Result.where(user_id: current_user.id)
 
     @analysis = Hash.new(0)
