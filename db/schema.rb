7680f880c97b0498be1e4297242f53dfbb2015f8
shiotomo <t.shiota16@gmail.com>
2018-09-30 03:06:35 +0900

    Codeモデルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724051308) do
+ActiveRecord::Schema.define(version: 20180929180448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20180724051308) do
     t.bigint "question_id"
     t.index ["category_id"], name: "index_category_items_on_category_id"
     t.index ["question_id"], name: "index_category_items_on_question_id"
+  end
+
+  create_table "codes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "code"
+    t.string "language"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
