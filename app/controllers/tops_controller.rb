ce64d8a5bcecfc48a017817de096d2f0b7b7ba33
shiotomo <t.shiota16@gmail.com>
2018-10-01 03:08:32 +0900

    利用コードを降順に表示するようにした

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,16 +4,19 @@ class TopsController < ApplicationController
   def index
     @questions = Question.all
     # @results = Result.where(user_id: current_user.id)
-    @results = current_user.results
-    @codes =  current_user.codes
+    results = current_user.results
+    codes =  current_user.codes
 
-    @analysis = Hash.new(0)
-    @results.each do |result|
-      @analysis[result.language] += 1
+    analysis = Hash.new(0)
+
+    results.each do |result|
+      analysis[result.language] += 1
     end
 
-    @codes.each do |code|
-      @analysis[code.language] += 1
+    codes.each do |code|
+      analysis[code.language] += 1
     end
+
+    @analysis = analysis.sort_by{|k, v| v}.reverse
   end
 end
