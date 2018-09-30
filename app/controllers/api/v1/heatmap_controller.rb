7177774483ba2346a51a747bed107a536e3099e2
shiotomo <t.shiota16@gmail.com>
2018-09-30 15:13:36 +0900

    APIの仕様を変更した

--- app/controllers/api/v1/heatmap_controller.rb
+++ app/controllers/api/v1/heatmap_controller.rb
@@ -5,9 +5,10 @@ class Api::V1::HeatmapController < Api::ApiController
   # heatmapのデータを返却するためのメソッド
   def index
     codes = current_user.codes.all
-    results = current_user.codes.all
-    graph = results.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}
-    graph.merge(codes.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}){|k, v1, v2| v1 + v2}
+    results = current_user.results.all
+    code_graph = codes.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}
+    result_graph = results.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}
+    graph = result_graph.merge(code_graph){|k, v1, v2| v1 + v2}
     render json: graph.to_json
   end
 
@@ -15,8 +16,9 @@ class Api::V1::HeatmapController < Api::ApiController
     user = User.friendly.find(params[:id])
     codes = Code.where(user_id: user.id)
     results = Result.where(user_id: user.id)
-    graph = codes.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}
-    graph.merge(results.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}){|k, v1, v2| v1 + v2}
+    code_graph = codes.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}
+    result_graph = results.map{|c| c.created_at.to_i}.inject(Hash.new(0)){|h, tm| h[tm] += 1; h}
+    graph = result_graph.merge(code_graph){|k, v1, v2| v1 + v2}
     render json: graph.to_json
   end
 end
