b740c68d742bcbde89e2b07ffa812db9d41fcad0
shiotomo <t.shiota16@gmail.com>
2018-10-08 01:02:02 +0900

    exec_data変数をParameterクラスに移行した

--- lib/code_candy/parameter.rb
+++ lib/code_candy/parameter.rb
@@ -20,6 +20,26 @@ module CodeCandy
         }
         return submit_language[:"#{language}"]
       end
+
+      def get_exec_data
+        exec_data = {
+          "Ruby":    Ruby.new,
+          "Python3": Python3.new,
+          "Gcc":     Gcc.new,
+          "Clang":   Clang.new,
+          "Golang":  Golang.new,
+          "Nodejs":  Nodejs.new,
+          "Java":    Java.new,
+          "Scala":   Scala.new,
+          "Swift":   Swift.new,
+          "CPP":     CPP.new,
+          "PHP":     PHP.new,
+          "Perl":    Perl.new,
+          "Bash":    Bash.new,
+          "Lua":     Lua.new
+        }
+        return exec_data
+      end
     end
   end
 end
