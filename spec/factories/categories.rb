34c2c29c909bd3ba019bd5fe476ba9f303d6c900
shiotomo <t.shiota16@gmail.com>
2018-11-10 19:33:46 +0900

    コード一覧を確認できるページを実装した #131

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,10 +2,11 @@
 #
 # Table name: categories
 #
-#  id         :integer          not null, primary key
+#  id         :bigint(8)        not null, primary key
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  comment    :text
 #
 
 FactoryBot.define do
