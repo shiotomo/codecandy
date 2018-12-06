9a430da218d7d1f681c8e5f58bb116424e41e0e3
shiotomo <t.shiota16@gmail.com>
2018-12-07 04:55:16 +0900

    code_golf_rankingの機能を実装した #138

--- app/controllers/code_golf_rankings_controller.rb
+++ app/controllers/code_golf_rankings_controller.rb
@@ -1,3 +1,5 @@
+require './lib/code_candy/parameter'
+
 class CodeGolfRankingsController < ApplicationController
   before_action :authenticate_user!
 
@@ -6,5 +8,17 @@ class CodeGolfRankingsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
+    @results = @question.results
+
+    # ランキングを格納するハッシュ
+    @ranking_hash = {}
+
+    # ランキングを格納(尚、使われていない言語のランキングは出てこない)
+    @results.each do |result|
+      next unless result.answer
+      @ranking_hash[:"#{result.language}"] = result unless @ranking_hash.has_key?(:"#{result.language}")
+      @ranking_hash[:"#{result.language}"] = result if @ranking_hash[:"#{result.language}"].code.length > result.code.length
+    end
   end
 end
