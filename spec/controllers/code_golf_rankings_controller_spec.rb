3df25d6482f8b952efa457fee3769f2626c3ca30
shiotomo <t.shiota16@gmail.com>
2018-12-14 01:47:44 +0900

    テストコードを追加した

--- spec/controllers/code_golf_rankings_controller_spec.rb
+++ spec/controllers/code_golf_rankings_controller_spec.rb
@@ -3,17 +3,20 @@ require 'rails_helper'
 RSpec.describe CodeGolfRankingsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'ログインしていない' do
+      it "returns http redirect" do
+        get :index
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    context 'ログインしていない' do
+      it "returns http redirect" do
+        get :show, params: { id: 1 }
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
-
 end
