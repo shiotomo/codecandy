9da8a3bf2b76a74ee01f4472d7e4ce8a40e525b9
shiotomo <t.shiota16@gmail.com>
2018-12-23 01:52:22 +0900

    テストを修正した #161

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe AdminsController, type: :controller do
     end
   end
 
-  describe "GET #show" do
+  describe "GET #user" do
     context 'ログインしていない' do
       it "returns http redirect" do
-        get :show, params: { id: 1 }
+        get :user, params: { id: 1 }
         expect(response).to have_http_status(:redirect)
       end
     end
