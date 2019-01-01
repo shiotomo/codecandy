41e0afd4a4a86c811227dcb15f3e795122793b90
shiotomo <t.shiota16@gmail.com>
2019-01-01 23:26:28 +0900

    heatmapの表示を変更した #174

--- vendor/assets/javascripts/heatmap.js
+++ vendor/assets/javascripts/heatmap.js
@@ -1,7 +1,18 @@
 const cal = new CalHeatMap();
 const now = new Date();
-const yearSelect = document.getElementById('year-select')
+// const yearSelect = document.getElementById('year-select')
 
+cal.init({
+  itemSelector: '#cal-heatmap',
+  domain: 'month',
+  data: `/api/v1/heatmap/data/${now.getFullYear()}`,
+  subdomain: 'day',
+  start: new Date(now.getFullYear(), 1, 0),
+  domainLabelFormat: '%Y-%m',
+  displayLegend: false
+});
+
+/*
 const response = fetch('/api/v1/user/period', {
   method: 'GET',
   headers: {
@@ -30,9 +41,11 @@ const response = fetch('/api/v1/user/period', {
   cal.init({
     itemSelector: '#cal-heatmap',
     domain: 'month',
-    data: `/api/v1/heatmap/data/${now.getFullYear()}`,
+    // data: `/api/v1/heatmap/data/${nowYear}`,
+    data: `/api/v1/heatmap/data/2018`,
     subdomain: 'day',
-    start: new Date(submitYear, 1, 0),
+    start: new Date(2018, 1, 0),
+    domainLabelFormat: '%Y-%m',
     displayLegend: false
   });
 
@@ -40,12 +53,14 @@ const response = fetch('/api/v1/user/period', {
 }).catch(err => {
   console.error(err);
 });
-
+*/
 
 /**
  * calheatmapの更新
  **/
+/*
 $('#year-select').change(() => {
+  // console.log(yearSelect.value)
   fetch(`/api/v1/heatmap/data/${yearSelect.value}`, {
     method: 'GET',
     headers: {
@@ -55,8 +70,11 @@ $('#year-select').change(() => {
     return response.json();
   }).then(jsonData => {
     console.log(jsonData);
-    cal.update(jsonData, true, cal.RESET_ALL_ON_UPDATE)
+    cal.update(jsonData, true, cal.RESET_ALL_ON_UPDATE);
+    // cal.update(jsonData);
+    // console.log(cal);
   }).catch(err => {
     console.error(err);
   });
 });
+*/
