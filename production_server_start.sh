0f38e5bf8f568b69ccc11362c53e55bdb96277a7
shiotomo <t.shiota16@gmail.com>
2019-02-07 03:12:57 +0900

    production環境用のコンテナを作成した #163

--- production_server_start.sh
+++ production_server_start.sh
@@ -3,5 +3,5 @@
 export SECRET_KEY_BASE=`bundle exec rails secret`
 bundle exec rails assets:precompile RAILS_ENV=production
 bin/webpack
-bundle exec rails s -b=0.0.0.0 -e production
+bundle exec rails s -e production
 # RAILS_ENV=production bundle exec pumactl start
