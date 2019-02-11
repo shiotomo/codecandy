ddc9221f205e6e269c136a0f96412a26fd5ece15
shiotomo <t.shiota16@gmail.com>
2019-02-12 08:00:15 +0900

    api/admin_controller.rbの一部機能をadmin_twitter.rbに移行した

---
+++ spec/lib/twitter/admin_twitter_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require './lib/twitter/admin_twitter'
+
+describe "admin_twitter" do
+  context "test環境の時" do
+    it "statusは'This environment is not production.'を返却する'", admin_twitter: true do
+      result = Twitter::AdminTwitter.tweet("hoge")
+      expect(result[:status]).to eq "This environment is not production."
+    end
+
+    it "is_successはfalseを返却する'", admin_twitter: true  do
+      result = Twitter::AdminTwitter.tweet("hoge")
+      expect(result[:is_success]).to eq false
+    end
+  end
+end
